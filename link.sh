0a766597c70678050cab34509e7a8b51fc033bf2
James Goppert <james.goppert@gmail.com>
2018-12-29 20:54:51 -0500

    Update link.sh

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ~/bin
-sudo apt-get install vim-gnome-py2
+sudo apt-get install vim
 ln -s /usr/bin/vim.gnome-py2  ~/bin/vi
 ln -s /usr/bin/vim.gnome-py2  ~/bin/vim
 ln -s $PWD/vimrc ~/.vimrc
